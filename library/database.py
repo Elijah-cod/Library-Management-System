b4d209ad82a441f608ecc05de252947fbb9eef8c
Elijah <iamngunjiri@gmail.com>
2025-03-14 09:52:58 +0300

    Done with the installations and creating the Database

---
+++ library/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# SQLite Database Connection
+DATABASE_URL = "sqlite:///library.db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+def get_db():
+    return SessionLocal()
